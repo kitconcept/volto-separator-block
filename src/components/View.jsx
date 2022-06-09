c169d16e43f37036186541fa03f9ed9009f8c1db
Steffen <ring@kitconcept.com>
2022-06-09 16:53:14 +0200

    eslint

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import cx from 'classnames';
 
 const SeparatorView = (props) => {
+  //eslint-disable-next-line
   const { className, data } = props;
   return (
     <div className={cx('block separator', className)}>
