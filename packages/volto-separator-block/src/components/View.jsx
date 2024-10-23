4135c8e88bd4851d22635f9189e1608c0cc1334c
Dante Álvarez <89805481+danalvrz@users.noreply.github.com>
2024-10-23 15:55:02 -0600

    fix short line width & update dependencies

--- packages/volto-separator-block/src/components/View.jsx
+++ packages/volto-separator-block/src/components/View.jsx
@@ -3,19 +3,18 @@ import cx from 'classnames';
 import { MaybeWrap } from '@plone/volto/components';
 
 const LegacyWrapper = (props) => (
-  <div className={cx('block separator', props.className)} style={props.style}>{children}</div>
+  <div className={cx('block separator', props.className)} style={props.style}>
+    {props.children}
+  </div>
 );
 
 const SeparatorView = (props) => {
   const { blocksConfig } = props;
   const isBlockModelv3 = blocksConfig?.separator?.blockModel === 3;
+
   return (
-    <MaybeWrap
-      condition={!isBlockModelv3}
-      as={LegacyWrapper}
-      {...props}
-    >
-      <div className="line testclass" />
+    <MaybeWrap condition={!isBlockModelv3} as={LegacyWrapper} {...props}>
+      <div className="line" />
     </MaybeWrap>
   );
 };
